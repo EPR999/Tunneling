a51c2c945302511b8fef1ad03f026a077bcc6cf4
EPR999 <qbit98631@gmail.com>
2018-11-11 11:57:27 +0900

    Update diff.py

--- diff.py
+++ diff.py
@@ -1,8 +1,7 @@
 import numpy as np
-import time 
 
-#線形化を行うために微分のところを実装する試み.
-#This program is the trial which concerns about differntiation of the map in order to do linearlization around fixed (periodic) point.
+#線形化を行うために写像についてヤコビアンを求める.
+#This program evaluate Jacobian of the map at fixed and periodic orbits in order to do linearlization around that point.
 xb = 1
 k = 1.2
 e2 = 1
@@ -26,14 +25,12 @@ def derivative(func,x,i):#多変数関数の微分(i番目が微分され戻る.
     return (np.array(func(xp))-np.array(func(xm)))/(2 * h)
 
 def main():
-    start = time.time() 
-    x = np.array([0.0,0.0]) #x = [Re(q),Re(p)] 不動点・周期点を受け取る.
+    x = np.array([0.0,0.0]) #x = [Re(q),Re(p)] 不動点・周期点を受け取る.#This get
     jacobian = [np.array([]),np.array([])]
     for i in range(dimension):#theta is one of the misterious
        jacobian =  [np.append(jacobian[j],derivative(U,x,i)[j]) for j in range(dimension)]
        print(jacobian)
     print(jacobian)#ヤコビアンが求まる.次は固有値を
-    elapsed_time = time.time() -start
     print("elapsed_time:{0}".format(elapsed_time)+"[sec]")
     w,v = np.linalg.eig(jacobian)#wは固有値,vは固有ベクトル.
     print(v)
